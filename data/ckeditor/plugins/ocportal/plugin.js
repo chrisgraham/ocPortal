2c9f9bfaf55a21c72b1f1a053951ecb3a2244f0b
Chris Graham <chris@ocproducts.com>
2011-11-29 19:03:29 +0000

    Whoops, commiting some new files missed out of the last few commits

---
+++ data/ckeditor/plugins/ocportal/plugin.js
@@ -0,0 +1,29 @@
+﻿(function() {
+	CKEDITOR.plugins.add('ocportal', {
+		init: function(editor) {
+			var possibles=['block','comcode','page','quote','box','code'];
+			for (var i=0;i<possibles.length;i++)
+			{
+				var buttonName=possibles[i];
+				var elements=get_elements_by_class_name(editor.element.$.parentNode.parentNode,'comcode_button_'+buttonName);
+				if (typeof elements[0]!='undefined')
+				{
+					var func= {
+						exec: elements[0].onclick
+					};
+					var label=elements[0].title;
+					var icon='plugins/ocportal/images/'+buttonName+'.png';
+
+					editor.addCommand('ocportal_'+buttonName,func);
+					editor.ui.addButton('ocportal_'+buttonName,{
+						label: label,
+						icon: icon,
+						command: 'ocportal_'+buttonName
+					});
+					
+					elements[0].parentNode.parentNode.style.display='none';
+				}
+			}
+		}
+	});
+})();
