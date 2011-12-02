50fed53d065f804ebd1bf91b58385c01f43da7d7
Chris Graham <chris@ocproducts.com>
2011-12-02 11:38:01 +0000

    fixed small bug in password reset process, if done in unusual order

--- pages/modules/lostpassword.php
+++ pages/modules/lostpassword.php
@@ -144,7 +144,7 @@ class Module_lostpassword
 		$zone=get_module_zone('join');
 		$_url=build_url(array('page'=>'lostpassword','type'=>'step3','code'=>$code,'member'=>$member),$zone,NULL,false,false,true);
 		$url=$_url->evaluate();
-		$_url_simple=build_url(array('page'=>'lostpassword','type'=>'step3'),$zone,NULL,false,false,true);
+		$_url_simple=build_url(array('page'=>'lostpassword','type'=>'step3','code'=>NULL,'username'=>NULL,'member'=>NULL),$zone,NULL,false,false,true);
 		$url_simple=$_url_simple->evaluate();
 		$message=do_lang('RESET_PASSWORD_TEXT',comcode_escape(get_site_name()),comcode_escape($username),array(comcode_escape($url),$url_simple,strval($member),strval($code)),get_lang($member));
 		require_code('mail');
