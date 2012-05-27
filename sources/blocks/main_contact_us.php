4659e54325df4e885add7300b1b841a788f88ce9
Chris Graham <chris@ocproducts.com>
2012-05-27 20:49:57 +0100

    Commited fix to issue #522 (http://ocportal.com/tracker/view.php?id=522). [main_contact_us block problem]

--- sources/blocks/main_contact_us.php
+++ sources/blocks/main_contact_us.php
@@ -82,6 +82,7 @@ class Block_main_contact_us
 			$email_from=trim(post_param('email',$GLOBALS['FORUM_DRIVER']->get_member_email_address(get_member())));
 			if ($email_from!='')
 			{
+				require_code('mail');
 				mail_wrap(do_lang('YOUR_MESSAGE_WAS_SENT_SUBJECT',$title),do_lang('YOUR_MESSAGE_WAS_SENT_BODY',$post),array($email_from),NULL,'','',3,NULL,false,get_member());
 			}
 		} else
