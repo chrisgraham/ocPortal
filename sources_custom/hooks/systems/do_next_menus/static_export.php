cd777f1567aee83ca5c042fe88338997b0bbc63e
Chris Graham <chris@ocproducts.com>
2012-06-20 04:06:14 +0100

    Deleting some files that should not be in git

---
+++ sources_custom/hooks/systems/do_next_menus/static_export.php
@@ -0,0 +1,34 @@
+<?php /*
+
+ ocPortal
+ Copyright (c) ocProducts, 2004-2012
+
+ See text/EN/licence.txt for full licencing information.
+
+*/
+
+/**
+ * @license		http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright	ocProducts Ltd
+ * @package		static_export
+ */
+
+
+class Hook_do_next_menus_static_export
+{
+
+	/**
+	 * Standard modular run function for do_next_menu hooks. They find links to put on standard navigation menus of the system.
+	 *
+	 * @return array			Array of links and where to show
+	 */
+	function run()
+	{
+		return array(
+			array('Tools','admin_home',array('static_export',array(),'adminzone'),make_string_tempcode('Export static site copy'))
+		);
+	}
+
+}
+
+
