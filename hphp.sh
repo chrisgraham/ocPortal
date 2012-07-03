cef90d4ac6787276d0730a226d3b2eefb2e74141
Chris Graham <chris@ocportal.com>
2012-07-03 20:08:17 +0100

    Fix to hphp build

--- hphp.sh
+++ hphp.sh
@@ -8,25 +8,18 @@
 	export MAKEOPTS=-j4
 #fi
 
-# If called as "sh hphp.sh continue" it will not rebuild the file list, it will just jump back into cmake
-if [ "$1" == "continue" ]
+# If called as "sh hphp.sh continue" it will not rebuild the file list, it will just jump back into build
+if [ "$1" != "continue" ]
 then
-	cd hphp
-	cmake .
-	exit
-fi
-
-#if [ ! -e "hphp.files.list" ]
-#then
 	echo "ocP: Finding files to compile..."
 	find . -name "*.php" -not -name errorlog.php -not -name phpstub.php -not -name caches_wincache.php -not -name caches_xcache.php -not -name caches_eaccelerator.php -not -name install.php -not -name minikernel.php -not -name phpbb3.php -not -name tempcode.php -not -name tempcode_compiler.php -not -name mysqli.php -not -name access.php -not -name sqlite.php -not -name mysql_dbx.php -not -name postgresql.php -not -name oracle.php -not -name sqlserver.php -not -name ibm.php | egrep -v '/_tests' | egrep -v '/adminzone/pages/minimodules_custom/' | egrep -v '/sources/hooks/modules/admin_import/' | egrep -v '/sources/forum/' | egrep -v '/sources_custom/hooks/systems/addon_registry/' > hphp.files.list
 	echo "./sources/forum/ocf.php" >> hphp.files.list
-#fi
 
-# compile_in_ocportal_includes.php is an ocPortal hook for additional specification of what files to include in hphp.files.list
-if [ -e "compile_in_ocportal_includes.php" ]
-then
-	php compile_in_ocportal_includes.php
+	# compile_in_ocportal_includes.php is an ocPortal hook for additional specification of what files to include in hphp.files.list
+	if [ -e "compile_in_ocportal_includes.php" ]
+	then
+		php compile_in_ocportal_includes.php
+	fi
 fi
 
 echo "ocP: Compiling..."
