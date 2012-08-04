4c6f042534da2125be423d23c4fd38bc6fa1290e
Chris Graham <chris@ocportal.com>
2012-08-04 23:01:20 +0100

    Whoops, fixing that push_bugfix did not work right

--- adminzone/pages/minimodules_custom/push_bugfix.php
+++ adminzone/pages/minimodules_custom/push_bugfix.php
@@ -1,2 +1,3 @@
 <?php // This just redirects to where they are in Dropbox for v8. In v9 this file is an integrated implementation
 header('Location: http://localhost/Dropbox/ocproducts/toolkit/v8-build-tools/push_bugfix.php');
+exit();
