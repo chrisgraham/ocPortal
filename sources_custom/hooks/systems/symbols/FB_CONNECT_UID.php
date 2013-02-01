731693016b42fd0eb2f802dfdcd4df397c9fd35a
World Wide Web Server <_www@Chris-Grahams-iMac-2.local>
2013-02-01 15:08:15 +0000

    Commited fix to issue #1002 (http://ocportal.com/tracker/view.php?id=1002). [Facebook users may need to refresh to have their login activate]

--- sources_custom/hooks/systems/symbols/FB_CONNECT_UID.php
+++ sources_custom/hooks/systems/symbols/FB_CONNECT_UID.php
@@ -7,17 +7,14 @@ class Hook_symbol_FB_CONNECT_UID
 		$value='';
 		if (get_forum_type()=='ocf')
 		{
-			if (!is_guest()) // A little crazy, but we need to do this as FB does not expire the cookie consistently, although oauth would have failed when creating a session against it
+			require_code('facebook_connect');
+			global $FACEBOOK_CONNECT;
+			if (!is_null($FACEBOOK_CONNECT))
 			{
-				require_code('facebook_connect');
-				global $FACEBOOK_CONNECT;
-				if (!is_null($FACEBOOK_CONNECT))
-				{
-					@ini_set('ocproducts.type_strictness','0');
-					$value=strval($FACEBOOK_CONNECT->getUser());
-					@ini_set('ocproducts.type_strictness','1');
-					if ($value=='0') $value='';
-				}
+				@ini_set('ocproducts.type_strictness','0');
+				$value=strval($FACEBOOK_CONNECT->getUser());
+				@ini_set('ocproducts.type_strictness','1');
+				if ($value=='0') $value='';
 			}
 		}
 		return $value;
