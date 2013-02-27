42d589b3f944a701cdea6e6df94295f61b61aa06
Chris Graham <chris@ocportal.com>
2013-02-27 13:08:20 +0000

    Fix after prior involved topics optimisation

--- forum/pages/modules/vforums.php
+++ forum/pages/modules/vforums.php
@@ -237,7 +237,9 @@ class Module_vforums
 			}
 			if (!is_guest())
 				$query.=' LEFT JOIN '.$GLOBALS['FORUM_DB']->get_table_prefix().'f_read_logs l ON (top.id=l.l_topic_id AND l.l_member_id='.strval(get_member()).')';
-			$query.=' LEFT JOIN '.$GLOBALS['FORUM_DB']->get_table_prefix().'translate t ON '.db_string_equal_to('language',user_lang()).' AND top.t_cache_first_post=t.id WHERE (('.$_condition.')'.$extra.') AND t_forum_id IS NOT NULL ORDER BY '.$order;
+			$query.=' LEFT JOIN '.$GLOBALS['FORUM_DB']->get_table_prefix().'translate t ON '.db_string_equal_to('language',user_lang()).' AND top.t_cache_first_post=t.id WHERE (('.$_condition.')'.$extra.') AND t_forum_id IS NOT NULL';
+			if (can_arbitrary_groupby()) $query.=' GROUP BY top.id';
+			$query.=' ORDER BY '.$order;
 			$topic_rows=array_merge($topic_rows,$GLOBALS['FORUM_DB']->query('SELECT top.*,t.text_parsed AS _trans_post,'.(is_guest()?'NULL as l_time':'l_time').$query,$max,$start));
 			//if (($start==0) && (count($topic_rows)<$max)) $max_rows+=$max; // We know that they're all on this screen
 			/*else */$max_rows+=$GLOBALS['FORUM_DB']->query_value_null_ok_full('SELECT COUNT(*) '.$query);
