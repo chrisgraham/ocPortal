50cf8e26dda6f30f32672f0a4a83ce199dea21a7
World Wide Web Server <_www@Chris-Grahams-iMac-2.local>
2013-03-09 12:57:36 +0000

    Commited fix to issue #1074 (http://ocportal.com/tracker/view.php?id=1074). [Facebook syndication not working properly]

--- sources_custom/hooks/systems/profiles_tabs/activities.php
+++ sources_custom/hooks/systems/profiles_tabs/activities.php
@@ -75,7 +75,14 @@ class Hook_Profiles_Tabs_activities
 						$url_map['syndicate_start__'.$hook]=1;
 						$oauth_url=build_url($url_map,'_SELF',NULL,false,false,false,'tab__activities');
 						$ob->auth_set($member_id_of,$oauth_url);
-					}
+					} elseif (($ob->auth_is_set($member_id_of)) && (either_param('oauth_in_progress',NULL)===NULL) && (!$GLOBALS['IS_ACTUALLY_ADMIN']))
+					{
+						// Do a refresh to make sure the token is updated
+						$url_map=array('page'=>'_SELF','type'=>'view','id'=>$member_id_of,'oauth_in_progress'=>1);
+						$url_map['syndicate_start__'.$hook]=1;
+						$oauth_url=build_url($url_map,'_SELF',NULL,false,false,false,'tab__activities');
+						$ob->auth_set($member_id_of,$oauth_url);
+ 					}
 
 					$syndications[$hook]=array(
 						'SYNDICATION_IS_SET'=>$ob->auth_is_set($member_id_of),
