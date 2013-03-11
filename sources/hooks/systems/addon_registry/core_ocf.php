be78d3766e2e497005de24adf150eca7a4251104
Chris Graham <chris@ocportal.com>
2013-03-11 16:29:51 +0000

    Fixed password changed notification being removed with ocf_forum

--- sources/hooks/systems/addon_registry/core_ocf.php
+++ sources/hooks/systems/addon_registry/core_ocf.php
@@ -74,6 +74,7 @@ class Hook_addon_registry_core_ocf
 	{
 		return array(
 
+			'sources/hooks/systems/notifications/ocf_password_changed.php',
 			'sources/hooks/systems/snippets/email_exists.php',
 			'sources/hooks/systems/snippets/profile_tab.php',
 			'sources/hooks/systems/snippets/invite_missing.php',
