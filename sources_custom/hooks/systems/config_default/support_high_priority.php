fe7b1825fc1bd87f2b9c8c2b18b882a846657060
Duck <WhomKnows@gmail.com>
2013-03-20 13:38:05 -0400

    Added Config default hooks for new customers config options.

---
+++ sources_custom/hooks/systems/config_default/support_high_priority.php
@@ -0,0 +1,51 @@
+<?php /*
+
+ ocPortal
+ Copyright (c) ocProducts, 2004-2012
+
+ See text/EN/licence.txt for full licencing information.
+
+*/
+
+/**
+ * @license		http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright	ocProducts Ltd
+ * @package		ocportalcom_support_credits
+ */
+
+class Hook_config_default_support_high_priority
+{
+
+	/**
+	 * Gets the details relating to the config option.
+	 *
+	 * @return ?array		The details (NULL: disabled)
+	 */
+	function get_details()
+	{
+		return array(
+			'human_name'=>'SUPPORT_PRIORITY_HIGH_MINUTES',
+			'the_type'=>'float',
+			'the_page'=>'FEATURE',
+			'section'=>'SECTION_CUSTOMERS',
+			'explanation'=>'CONFIG_OPTION_support_high_priority',
+			'shared_hosting_restricted'=>'0',
+			'c_data'=>'',
+
+			'addon'=>'core_configuration',
+		);
+	}
+
+	/**
+	 * Gets the default value for the config option.
+	 *
+	 * @return ?string		The default value (NULL: option is disabled)
+	 */
+	function get_default()
+	{
+		return '5';
+	}
+
+}
+
+
