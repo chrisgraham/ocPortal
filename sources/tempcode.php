44f2a90cdce452156415bf7e307a6e1e6fa731d6
Chris Graham <chris@ocportal.com>
2013-08-14 23:14:54 +0100

    Small flexibility tweak

--- sources/tempcode.php
+++ sources/tempcode.php
@@ -910,7 +910,7 @@ function handle_symbol_preprocessing($bit,&$children)
 				{
 					$before=memory_get_usage();
 				}
-				$tp_value=request_page($param[0],false,array_key_exists(1,$param)?$param[1]:NULL,NULL,$being_included);
+				$tp_value=request_page($param[0],false,array_key_exists(1,$param)?$param[1]:get_comcode_zone($param[0],false),NULL,$being_included);
 				if ((function_exists('memory_get_usage')) && (isset($_GET['keep_show_loading'])) && ($_GET['keep_show_loading']=='1'))
 				{
 					@ob_end_flush();
