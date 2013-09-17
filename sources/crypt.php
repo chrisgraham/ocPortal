ef2290b6af8bd8b92a32bcc68d7386efee26ee47
Chris Graham <chris@ocportal.com>
2013-09-17 23:47:10 +0100

    Fix issue with crypto improvements

--- sources/crypt.php
+++ sources/crypt.php
@@ -124,13 +124,14 @@ function get_rand_password()
  */
 function get_secure_random_number()
 {
+	// 2147483647 is from MySQL limit http://dev.mysql.com/doc/refman/5.0/en/integer-types.html ; PHP_INT_MAX is higher on 64bit machines
 	if ((function_exists('openssl_random_pseudo_bytes')) && (get_value('disable_openssl')!=='1'))
 	{
-		$code=intval(floor(PHP_INT_MAX*(hexdec(bin2hex(openssl_random_pseudo_bytes(4)))/0xffffffff)));
+		$code=intval(floor(2147483647*(hexdec(bin2hex(openssl_random_pseudo_bytes(4)))/0xffffffff)));
 		if ($code<0) $code=-$code;
 	} else
 	{
-		$code=mt_rand(0,mt_getrandmax());
+		$code=mt_rand(0,min(2147483647,mt_getrandmax()));
 	}
 	return $code;
 }
