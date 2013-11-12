b9133c8af926e185f330b8a9f08a0f51c561d7f2
Chris Graham <chris@ocportal.com>
2013-11-12 15:59:14 +0000

    Field API fixes

--- sources/hooks/systems/fields/short_trans.php
+++ sources/hooks/systems/fields/short_trans.php
@@ -118,7 +118,7 @@ class Hook_fields_short_trans
 	{
 		$id=$field['id'];
 		$tmp_name='field_'.strval($id);
-		return post_param($tmp_name,STRING_MAGIC_NULL);
+		return post_param($tmp_name,$editing?STRING_MAGIC_NULL:'');
 	}
 
 }
