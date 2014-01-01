a4919a822e417d28bcf2b67b2dca5ec7aff65e0a
Chris Graham <chris@ocportal.com>
2014-01-01 23:07:43 +0000

    Changed birthday algorithm to account for uneven birthdays, lazy people set 1st Jan instead of the real thing

--- sources/ocf_general.php
+++ sources/ocf_general.php
@@ -187,10 +187,11 @@ function ocf_find_birthdays($time=NULL)
 {
 	if (is_null($time)) $time=time();
 
-	$num_members=$GLOBALS['FORUM_DB']->query_value('f_members','COUNT(*)');
-	if ($num_members>365*20) return array(); // 20 birthdays on average per day is more than worth reporting! And would kill performance
-
 	list($day,$month,$year)=explode(' ',date('j m Y',utctime_to_usertime($time)));
+
+	$num_members=$GLOBALS['FORUM_DB']->query_value('f_members','COUNT(*)',array('m_dob_day'=>intval($day),'m_dob_month'=>intval($month)));
+	if ($num_members>20) return array();
+
 	$rows=$GLOBALS['FORUM_DB']->query_select('f_members',array('id','m_username','m_reveal_age','m_dob_year'),array('m_dob_day'=>intval($day),'m_dob_month'=>intval($month)));
 
 	$birthdays=array();
