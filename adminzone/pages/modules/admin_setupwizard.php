3367e0e7493db7f922583abf73d21482d2be18f6
World Wide Web Server <_www@Chris-Grahams-iMac-2.local>
2014-01-04 02:25:42 +0000

    Commited fix to issue #1493 (http://ocportal.com/tracker/view.php?id=1493). [Setup Wizard problem on Windows]

--- adminzone/pages/modules/admin_setupwizard.php
+++ adminzone/pages/modules/admin_setupwizard.php
@@ -798,7 +798,7 @@ class Module_admin_setupwizard
 					if ($addon_row['addon_files']!='')
 					{
 						$file=preg_replace('#^[\_\.\-]#','x',preg_replace('#[^\w\.\-]#','_',$addon_row['addon_name'])).'.tar';
-						create_addon($file,explode(chr(10),$addon_row['addon_files']),$addon_row['addon_name'],implode(',',$addon_row['addon_incompatibilities']),implode(',',$addon_row['addon_dependencies']),$addon_row['addon_author'],$addon_row['addon_organisation'],$addon_row['addon_version'],$addon_row['addon_description'],'imports/addons');
+						create_addon($file,explode(chr(10),trim($addon_row['addon_files'])),$addon_row['addon_name'],implode(',',$addon_row['addon_incompatibilities']),implode(',',$addon_row['addon_dependencies']),$addon_row['addon_author'],$addon_row['addon_organisation'],$addon_row['addon_version'],$addon_row['addon_description'],'imports/addons');
 					}
 
 					uninstall_addon($addon_row['addon_name']);
