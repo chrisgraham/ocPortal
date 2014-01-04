a76f3072b8f3356ce8d5745addf8c1d1eab91ba9
World Wide Web Server <_www@Chris-Grahams-iMac-2.local>
2014-01-04 01:49:08 +0000

    Commited fix to issue #1492 (http://ocportal.com/tracker/view.php?id=1492). [Problem with Comcode conversion]

--- sources/comcode_from_html.php
+++ sources/comcode_from_html.php
@@ -341,11 +341,11 @@ function semihtml_to_comcode($semihtml,$force=false)
 	{
 		foreach (array('&amp;thumb=0[^"]*'=>' thumb="0"','&amp;thumb=1[^"]*'=>' thumb="1"',''=>'') as $thumb_bit=>$comcode_to)
 		{
-			$semihtml=preg_replace('#<div [^>]*class="attachment_left"[^>]*>\s*<img src="[^"]*?id=(\d+)[^"]*'.$thumb_bit.'(&amp;for_session=[\w\d]+)?" /></div>#i','[attachment_safe'.$comcode_to.' type="inline_left"]${1}[/attachment_safe]',$semihtml);
-			$semihtml=preg_replace('#<div [^>]*class="attachment_right"[^>]*>\s*<img src="[^"]*?id=(\d+)[^"]*'.$thumb_bit.'(&amp;for_session=[\w\d]+)?" /></div>#i','[attachment_safe'.$comcode_to.' type="inline_right"]${1}[/attachment_safe]',$semihtml);
+			$semihtml=preg_replace('#<div [^>]*class="attachment_left"[^>]*>\s*<img src="[^"]*\?id=(\d+)[^"]*'.$thumb_bit.'(&amp;for_session=[\w\d]+)?" /></div>#i','[attachment_safe'.$comcode_to.' type="inline_left"]${1}[/attachment_safe]',$semihtml);
+			$semihtml=preg_replace('#<div [^>]*class="attachment_right"[^>]*>\s*<img src="[^"]*\?id=(\d+)[^"]*'.$thumb_bit.'(&amp;for_session=[\w\d]+)?" /></div>#i','[attachment_safe'.$comcode_to.' type="inline_right"]${1}[/attachment_safe]',$semihtml);
 			$regexp='#';
 			$regexp.=($comcode_to==' thumb="1"')?'(<a [^>]*>)?\s*':'()';
-			$regexp.='<img [^>]*[^>]*src="[^"]*?id=(\d+)[^"]*'.$thumb_bit.'(&amp;for_session=[\w\d]+)?" [^>]*title="([^"]*)" [^>]*/?'.'>\s*';
+			$regexp.='<img [^>]*src="[^"]*\?id=(\d+)[^"]*'.$thumb_bit.'(&amp;for_session=[\w\d]+)?" [^>]*title="([^"]*)" [^>]*/?'.'>\s*';
 			$regexp.=($comcode_to==' thumb="1"')?'(</a>)?':'()';
 			$regexp.='#i';
 			$semihtml=preg_replace($regexp,'[attachment_safe'.$comcode_to.' type="inline" description="${4}"]${2}[/attachment_safe]',$semihtml);
