aad910e9b9cec6d3bed712eb3748c361454706d5
Chris Graham <chris@ocportal.com>
2014-01-31 20:04:20 +0000

    Non-recurring usergroup UI fix

--- adminzone/pages/modules/admin_ecommerce.php
+++ adminzone/pages/modules/admin_ecommerce.php
@@ -37,16 +37,23 @@ class Module_admin_ecommerce extends standard_crud_module
 		var _length_units=document.getElementById('length_units'),_length=document.getElementById('length');
 		var adjust_lengths=function()
 		{
-			// Limits based on https://developer.paypal.com/docs/classic/paypal-payments-standard/integration-guide/Appx_websitestandard_htmlvariables/
 			var length_units=_length_units.options[_length_units.selectedIndex].value,length=_length.value;
-			if ((length_units=='d') && ((length<1) || (length>90)))
-				_length.value=(length<1)?1:90;
-			if ((length_units=='w') && ((length<1) || (length>52)))
-				_length.value=(length<1)?1:52;
-			if ((length_units=='m') && ((length<1) || (length>24)))
-				_length.value=(length<1)?1:24;
-			if ((length_units=='y') && ((length<1) || (length>5)))
-				_length.value=(length<1)?1:5;
+			if (document.getElementById('auto_recur').checked)
+			{
+				// Limits based on https://developer.paypal.com/docs/classic/paypal-payments-standard/integration-guide/Appx_websitestandard_htmlvariables/
+				if ((length_units=='d') && ((length<1) || (length>90)))
+					_length.value=(length<1)?1:90;
+				if ((length_units=='w') && ((length<1) || (length>52)))
+					_length.value=(length<1)?1:52;
+				if ((length_units=='m') && ((length<1) || (length>24)))
+					_length.value=(length<1)?1:24;
+				if ((length_units=='y') && ((length<1) || (length>5)))
+					_length.value=(length<1)?1:5;
+			} else
+			{
+				if (length<1)
+					_length.value=1;
+			}
 		}
 		_length_units.onchange=adjust_lengths;
 		_length.onchange=adjust_lengths;
