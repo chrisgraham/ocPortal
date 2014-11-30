7cd180a36f1f9a9b0df7dad57fbccebf06664206
World Wide Web Server <_www@Chriss-iMac.home>
2014-11-30 12:55:50 +0000

    Commited fix to issue #1732 (http://ocportal.com/tracker/view.php?id=1732). [Fixes/clear-ups for section and currency Comcode tags]

--- sources/comcode_add.php
+++ sources/comcode_add.php
@@ -326,7 +326,23 @@ function comcode_helper_script()
 		if (array_key_exists($tag,$tag_list))
 		{
 			$params=$tag_list[$tag];
-			if ($tag=='include')
+			if ($tag=='currency')
+			{
+				$default=array_key_exists('param',$defaults)?$defaults['param']:get_param('default_param',get_option('currency'));
+				$list=new ocp_tempcode();
+				require_code('currency');
+				foreach (array_keys(get_currency_map()) as $currency)
+				{
+					$list->attach(form_input_list_entry($currency,$currency==$default));
+				}
+				$fields->attach(form_input_list(do_lang_tempcode('CURRENCY'),do_lang_tempcode('COMCODE_TAG_currency_PARAM_param'),'param',$list));
+				$default_embed=array_key_exists('',$defaults)?($defaults['']):get_param('default','');
+				$fields->attach(form_input_float(do_lang_tempcode('AMOUNT'),do_lang_tempcode('COMCODE_TAG_currency_EMBED'),'tag_contents',floatval($default_embed),true));
+				$default=array_key_exists('bracket',$defaults)?$defaults['bracket']:get_param('default_bracket','');
+				$fields->attach(form_input_tick(titleify('bracket'),protect_from_escaping(ucfirst(substr(do_lang('COMCODE_TAG_currency_PARAM_bracket'),12))),'bracket',$default=='1'));
+				$done_tag_contents=true;
+			}
+			elseif ($tag=='include')
 			{
 				$default_embed=array_key_exists('',$defaults)?($defaults['']):get_param('default','');
 				if (strpos($default_embed,':')===false) $default_embed=':'.$default_embed;
@@ -760,7 +776,7 @@ function _get_preview_environment_comcode($tag)
 	{
 		$i=0;
 		$default=post_param_integer('default',0);
-		$comcode='';
+		$comcode='[surround]';
 		$controller=array();
 		while (post_param('tag_contents_'.strval($i),'')!='' && post_param('name_'.strval($i),'')!='')
 		{
@@ -776,7 +792,7 @@ function _get_preview_environment_comcode($tag)
 			$bparameters.="<section=\"$name\"$def>".$content."</section>";
 			$i++;
 		}
-		$comcode.='[section_controller]'.implode(',',$controller).'[/section_controller]';
+		$comcode.='[section_controller]'.implode(',',$controller).'[/section_controller][/surround]';
 		$bparameters.='<section_controller>'.implode(',',$controller).'</section_controller>';
 	}
 	elseif ($tag=='big_tabs')
--- themes/default/templates/COMCODE_SECTION.tpl
+++ themes/default/templates/COMCODE_SECTION.tpl
@@ -1,4 +1,4 @@
-<div class="comcode_section" id="{$FIX_ID,{PASS_ID|*}}_section_{NAME*}" style="display: {$?,{$OR,{DEFAULT},{$NOT,{$JS_ON}}},block,none}">
+<div class="comcode_section" id="{$FIX_ID,{PASS_ID|*}}_section_{$FIX_ID,{NAME*|}}" style="display: {$?,{$OR,{DEFAULT},{$NOT,{$JS_ON}}},block,none}">
 	{CONTENT}
 </div>
 
