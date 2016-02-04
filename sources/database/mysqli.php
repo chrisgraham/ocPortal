ea1ab37ba3a6ab411e4f8f9764a5493c9172c816
Chris Graham <chris@ocproducts.com>
2016-02-04 03:29:38 +0000

    9.0.24

--- sources/database/mysqli.php
+++ sources/database/mysqli.php
@@ -246,7 +246,7 @@ class Database_Static_mysqli extends Database_super_mysql
 			}
 
 			if (function_exists('ocp_mark_as_escaped')) ocp_mark_as_escaped($err);
-			if ((!running_script('upgrader')) && (!get_mass_import_mode()))
+			if ((!running_script('upgrader')) && (!get_mass_import_mode()) && (strpos($err,'Duplicate entry')===false))
 			{
 				if (!function_exists('do_lang') || is_null(do_lang('QUERY_FAILED',NULL,NULL,NULL,NULL,false))) fatal_exit(htmlentities('Query failed: '.$query.' : '.$err));
 
