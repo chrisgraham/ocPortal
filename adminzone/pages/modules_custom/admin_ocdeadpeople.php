6e9e40bd35eba7f78ef6045282e3c2c7727a3980
Chris Graham <chris@ocproducts.com>
2016-04-27 22:09:35 +0100

    Misc fixes

--- adminzone/pages/modules_custom/admin_ocdeadpeople.php
+++ adminzone/pages/modules_custom/admin_ocdeadpeople.php
@@ -22,7 +22,6 @@ class Module_admin_ocdeadpeople extends standard_aed_module
 {
 	var $lang_type='DISEASE';
 	var $select_name='NAME';
-	var $award_type='group';
 	var $possibly_some_kind_of_upload=true;
 	var $output_of_action_is_confirmation=true;
 	var $menu_label='OCDEADPEOPLE_TITLE';
