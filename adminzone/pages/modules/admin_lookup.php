fff62a5fcb2d3aa71dd3daaa48758804a1920112
Chris Graham <chris@ocproducts.com>
2016-05-22 11:05:35 -0400

    Misc fixes for automatic error mail issues

--- adminzone/pages/modules/admin_lookup.php
+++ adminzone/pages/modules/admin_lookup.php
@@ -114,7 +114,10 @@ class Module_admin_lookup
 			if (is_null($id)) $id=$GLOBALS['FORUM_DRIVER']->get_guest_id();
 			if (is_null($ip)) $ip='';
 
-			$all_banned=collapse_1d_complexity('ip',$GLOBALS['SITE_DB']->query('SELECT ip FROM '.get_table_prefix().'usersubmitban_ip WHERE i_ban_positive=1 AND (i_ban_until IS NULL OR i_ban_until>'.strval(time()).')'));
+			if (addon_installed('securitylogging'))
+				$all_banned=collapse_1d_complexity('ip',$GLOBALS['SITE_DB']->query('SELECT ip FROM '.get_table_prefix().'usersubmitban_ip WHERE i_ban_positive=1 AND (i_ban_until IS NULL OR i_ban_until>'.strval(time()).')'));
+			else
+				$all_banned=array();
 
 			$ip_list=new ocp_tempcode();
 			$groups=array();
@@ -203,7 +206,7 @@ class Module_admin_lookup
 
 			$member_banned=$GLOBALS['FORUM_DRIVER']->is_banned($id);
 			$ip_banned=false;
-			if ($ip!='')
+			if ($ip!='' && addon_installed('securitylogging'))
 			{
 				$ban_until=$GLOBALS['SITE_DB']->query_select('usersubmitban_ip',array('i_ban_until'),array('i_ban_positive'=>1,'ip'=>$ip));
 				if (array_key_exists(0,$ban_until))
